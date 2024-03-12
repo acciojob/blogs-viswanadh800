e8431db3de80b9d2e436c0afb5f11c9e6b50d8c1
Viswanadh <61613578+viswanadh800@users.noreply.github.com>
2024-03-12 14:12:02 +0530

    Update ImageController.java

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -12,14 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/images")
 public class ImageController {
 
+    @Autowired
+    ImageService imageService;
+    
     @PostMapping("/{blogId}/add-image")
     public ResponseEntity<String> addImage(@PathVariable int blogId, @RequestParam String description, @RequestParam String dimensions) {
         // Add image into the give blog
+        imageService.addImage(blogId,description,dimensions);
         return new ResponseEntity<>("Added image successfully", HttpStatus.OK);
     }
 
     @GetMapping("/countImagesInScreen/{id}/{screenDimensions}")
     public ResponseEntity<Integer> countImagesInScreen(@PathVariable int id, @PathVariable String screenDimensions){
+        int count=imageService.countImagesInScreen(id,screenDimensions);
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
